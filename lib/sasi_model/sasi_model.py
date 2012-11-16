d1d490fb5507a88c1d172ec5e3f84b9578a5760a
adorsk <adorsk>
2012-11-16 16:16:24 -0500

    changed to use dao, otherwise results get too big for memory

--- lib/sasi_model/sasi_model.py
+++ lib/sasi_model/sasi_model.py
@@ -1,10 +1,10 @@
+from sasi_data.dao.sasi_sa_dao import SASI_SqlAlchemyDAO
 import logging
 
 
 class SASI_Model(object):
 
-    def __init__(self, t0=0, tf=10, dt=1, taus=None, omegas=None,
-                 cells=None, features=None, efforts=None, vas=None,
+    def __init__(self, t0=0, tf=10, dt=1, taus=None, omegas=None, dao=None, 
                  logger=logging.getLogger()):
 
         self.logger = logger
@@ -32,34 +32,27 @@ class SASI_Model(object):
                     }
         self.omegas = omegas
 
-        self.cells = cells
-        self.features = features
-        self.efforts = efforts
-        self.vas = vas # Vulnerability Assessments
-
-        # Results, grouped by time and cell.
-        self.results_t_c = {}
-
-        # Results, as a list.
-        self.results = []
+        self.dao = dao
 
         self.setup()
 
     def setup(self):
         """
-        Create look-ups.
+        Create in-memory look-ups to speed things up.
         """
+
+        vas = self.dao.query('__VA')
 
         # Create va lookup.
         self.va_lu = {}
-        for va in self.vas:
+        for va in vas:
             key = (va.gear_id, va.substrate_id, va.energy_id, va.feature_id)
             self.va_lu[key] = va
 
         # Group habitat types by gears to which they can be applied.
         self.logger.info("Grouping habitats by gears...")
         self.ht_by_g = {} 
-        for va in self.vas:
+        for va in vas:
             ht = (va.substrate_id, va.energy_id,)
             gear_hts = self.ht_by_g.setdefault(va.gear_id, set())
             gear_hts.add(ht)
@@ -67,20 +60,20 @@ class SASI_Model(object):
         # Group features by gears to which they can be applied.
         self.logger.info("Grouping features by gears...")
         self.f_by_g = {}
-        for va in self.vas:
+        for va in vas:
             gear_fs = self.f_by_g.setdefault(va.gear_id, set())
             gear_fs.add(va.feature_id)
 
         # Create feature lookup.
         self.logger.info("Creating features lookup...")
         self.features_lu = {}
-        for f in self.features:
+        for f in self.dao.query('__Feature'):
             self.features_lu[f.id] = f
 
         # Group features by category and habitat types.
         self.logger.info("Grouping features by categories and habitats...")
         self.f_by_ht_fc = {}
-        for va in self.vas:
+        for va in vas:
             ht = (va.substrate_id, va.energy_id,)
             ht_fcs = self.f_by_ht_fc.setdefault(ht, {})
             f = self.features_lu[va.feature_id]
@@ -92,23 +85,13 @@ class SASI_Model(object):
         # Create cells-habitat_type-feature_category-feature lookup.
         self.c_ht_fc_f = self.get_c_ht_fc_f_lookup()
 
-        # Create effort lookup by cell and time.
-        self.c_t_e = self.get_c_t_e_lookup()
-
-        # Initialize results, grouped by time and cell.
-        self.logger.info("Initializing results...")
-        for t in range(self.t0, self.tf + self.dt, self.dt):
-            self.results_t_c[t] = {}
-            for c in self.c_ht_fc_f.keys():
-                self.results_t_c[t][c] = {}
-
     def get_c_ht_fc_f_lookup(self):
         """ Create cells-habitat_type-feature_category-feature lookup. """
 
         self.logger.info(("Creating cells-habitat_type-feature_category-"
                           "feature lookup..."))
         c_ht_fc_f = {}
-        for c in self.cells:
+        for c in self.dao.query('__Cell'):
             c_ht_fc_f[c.id] = { 'ht': {} }
             for ht, pct_area in c.habitat_composition.items():
                 c_ht_fc_f[c.id]['ht'][ht] = {
@@ -119,147 +102,164 @@ class SASI_Model(object):
                     c_ht_fc_f[c.id]['ht'][ht]['fc'][fc] = fs
         return c_ht_fc_f
 
-    def get_c_t_e_lookup(self):
-        """ Create cells-time-effort lookup. """
-
-        base_msg = "Creating cells-time-effort lookup..."
-        self.logger.info(base_msg)
-
-        c_t_e = {}
-        effort_counter = 0
-        num_efforts = len(self.efforts)
-        for e in self.efforts:
-            effort_counter += 1
-            if (effort_counter % 1000) == 0: 
-                self.logger.info(base_msg + "effort #%d of %d (%.1f%%)" % (
-                    effort_counter, num_efforts, 
-                    (1.0 * effort_counter/num_efforts) * 100
-                ))
-            c_t = (e.cell_id, e.time,)
-            es = c_t_e.setdefault(c_t, [])
-            es.append(e)
-        return c_t_e
-
-    def run(self):
-        for t in range(self.t0, self.tf + 1, self.dt):
-            self.iterate(t)
-
-    def iterate(self, t, log_interval=1000):
-        base_msg = "Iterating, time = %s..." % t
-        self.logger.info(base_msg)
-
-        result_counter = 0
-
+    def run(self, log_interval=100, commit=True):
+        # We partition by cells to avoid overloading memory.
+        # 'Cuz there can be a lotta data...
         cell_counter = 0
         cells = self.c_ht_fc_f.keys()
         num_cells = len(cells)
         for c in cells:
+
             cell_counter += 1
             if (cell_counter % log_interval) == 0: 
-               self.logger.info(base_msg + (" cell #%d of %d (%.1f%%)" % (
+               self.logger.info("cell #%d of %d (%.1f%%)" % (
                    cell_counter, num_cells, 1.0 * cell_counter/num_cells * 100))
-               )
 
-            cell_efforts = self.c_t_e.get((c,t),[])
+            # Get a local cache of results and efforts for the cell.
+            cell_results = self.get_results_for_cell_by_t_rkey(c)
+            cell_efforts = self.get_efforts_for_cell_by_t(c)
 
-            for effort in cell_efforts:
+            for t in range(self.t0, self.tf + 1, self.dt):
+                for effort in cell_efforts[t]:
 
-                relevant_habitat_types = []
-                for ht in self.c_ht_fc_f[c]['ht'].keys():
-                    if ht in self.ht_by_g[effort.gear_id]: 
-                        relevant_habitat_types.append(ht)
+                    # Get relevant habitat types for the effort.
+                    relevant_habitat_types = []
+                    for ht in self.c_ht_fc_f[c]['ht'].keys():
+                        if ht in self.ht_by_g[effort.gear_id]: 
+                            relevant_habitat_types.append(ht)
 
-                if relevant_habitat_types:
+                    if relevant_habitat_types:
+                        # Get combined area of relevant habitats.
+                        relevant_habitats_area = sum(
+                            [self.c_ht_fc_f[c]['ht'][ht]['area'] 
+                             for ht in relevant_habitat_types])
 
-                    relevant_habitats_area = sum(
-                        [self.c_ht_fc_f[c]['ht'][ht]['area'] 
-                         for ht in relevant_habitat_types])
+                        # For each relevant habitat...
+                        for ht in relevant_habitat_types:
 
-                    for ht in relevant_habitat_types:
+                            # Distribute the effort's swept area proportionally 
+                            # to the habitat type's area as a fraction of the 
+                            # total relevant area.
+                            ht_area = self.c_ht_fc_f[c]['ht'][ht]['area']
+                            ht_swept_area = effort.swept_area * (
+                                ht_area/relevant_habitats_area)
 
-                        # Distribute the effort's swept area proportionally 
-                        # to the habitat type's area as a fraction of the 
-                        # total relevant area.
-                        ht_area = self.c_ht_fc_f[c]['ht'][ht]['area']
-                        ht_swept_area = effort.swept_area * (
-                            ht_area/relevant_habitats_area)
+                            # Distribute swept area equally across feature categories.
+                            fcs = self.c_ht_fc_f[c]['ht'][ht]['fc'].keys()
+                            fc_swept_area = ht_swept_area/len(fcs)
 
-                        # Distribute swept area equally across feature categories.
-                        fcs = self.c_ht_fc_f[c]['ht'][ht]['fc'].keys()
-                        fc_swept_area = ht_swept_area/len(fcs)
+                            for fc in fcs:
 
-                        for fc in fcs:
+                                relevant_features = []
+                                for f in self.c_ht_fc_f[c]['ht'][ht]['fc'][fc]:
+                                    if f in self.f_by_g[effort.gear_id]: 
+                                        relevant_features.append(f)
 
-                            relevant_features = []
-                            for f in self.c_ht_fc_f[c]['ht'][ht]['fc'][fc]:
-                                if f in self.f_by_g[effort.gear_id]: 
-                                    relevant_features.append(f)
+                                if relevant_features:
 
-                            if relevant_features:
+                                    # Distribute the category's effort equally over the features.
+                                    f_swept_area = fc_swept_area/len(relevant_features)
 
-                                # Distribute the category's effort equally over the features.
-                                f_swept_area = fc_swept_area/len(relevant_features)
+                                    for f in relevant_features:
 
-                                for f in relevant_features:
+                                        # Get vulnerability assessment for the effort.
+                                        va = self.va_lu[(effort.gear_id, ht[0], ht[1], f)]
 
-                                    # Get vulnerability assessment for the effort.
-                                    va = self.va_lu[(effort.gear_id, ht[0], ht[1], f)]
+                                        omega = self.omegas[va.s]
+                                        tau = self.taus[va.r]
 
-                                    omega = self.omegas[va.s]
-                                    tau = self.taus[va.r]
+                                        result_key = (ht[0], ht[1], effort.gear_id, f)
+                                        result = self.get_or_create_result(
+                                            cell_results, t, c, result_key)
 
-                                    result_key = (ht[0], ht[1], effort.gear_id, f)
-                                    result = self.get_or_create_result(t, c, result_key)
+                                        # Add the resulting contact-adjusted
+                                        # swept area to the a field.
+                                        result.a += f_swept_area
 
-                                    # Add the resulting contact-adjusted
-                                    # swept area to the a field.
-                                    result['a'] += f_swept_area
+                                        # Calculate adverse effect swept area and add to y field.
+                                        adverse_effect_swept_area = f_swept_area * omega
+                                        result.y += adverse_effect_swept_area
 
-                                    # Calculate adverse effect swept area and add to y field.
-                                    adverse_effect_swept_area = f_swept_area * omega
-                                    result['y'] += adverse_effect_swept_area
+                                        # Calculate recovery per timestep.
+                                        if tau == 0:
+                                            recovery_per_dt = 0
+                                        else:
+                                            recovery_per_dt = adverse_effect_swept_area/tau
 
-                                    # Calculate recovery per timestep.
-                                    if tau == 0:
-                                        recovery_per_dt = 0
-                                    else:
-                                        recovery_per_dt = adverse_effect_swept_area/tau
+                                        # Add recovery to x field for future entries.
+                                        for future_t in range(t + 1, t + tau + 1, self.dt):
+                                            if future_t <= self.tf:
+                                                future_result = self.get_or_create_result(
+                                                    cell_results, future_t, c, result_key)
+                                                future_result.x += recovery_per_dt
 
-                                    # Add recovery to x field for future entries.
-                                    for future_t in range(t + 1, t + tau + 1, self.dt):
-                                        if future_t <= self.tf:
-                                            future_result = self.get_or_create_result(future_t, c, result_key)
-                                            future_result['x'] += recovery_per_dt
+                                        # Calculate Z.
+                                        result.z = result.x - result.y
 
-                                    # Calculate Z.
-                                    result['z'] = result['x'] - result['y']
+                                        # Update znet
+                                        result.znet += result.z
 
-                                    # Update znet
-                                    result['znet'] += result['z']
+                                # End of feature block
+                    # End of habitats block
+                # End of efforts block
+            # End of timestep block
 
-                                    # End of iteration.
+            # Save results.
+            for results in cell_results.values():
+                for result in results.values():
+                    self.dao.save(result, auto_commit=False)
+            if commit:
+                self.dao.commit()
+            # End of cell block
 
-    def get_or_create_result(self, t, cell_id, result_key):
+    def get_efforts_for_cell_by_t(self, cell_id):
+        """ Get efforts cell, grouped by time. """
+        effort_cache = {}
+        for t in range(self.t0, self.tf + 1, self.dt):
+            effort_cache[t] = self.dao.query({
+                'SELECT': '__Effort',
+                'WHERE': [[{'TYPE': 'ENTITY', 
+                            'EXPRESSION': '__Effort__cell_id'},
+                           '==', cell_id]],
+            })
+        return effort_cache
+
+    def get_results_for_cell_by_t_rkey(self, cell_id):
+        """ Get results cell, grouped by time and result key. """
+        result_cache = {}
+        for t in range(self.t0, self.tf + 1, self.dt):
+            result_cache[t] = {}
+        cell_results = self.dao.query({
+            'SELECT': '__Result',
+            'WHERE': [[{'TYPE': 'ENTITY', 
+                        'EXPRESSION': '__Result__cell_id'},
+                       '==', cell_id]],
+        })
+        for r in cell_results:
+            key = (r.substrate, r.energy, r.gear_id, r.feature)
+            result_cache[t][result_key] = result
+        return result_cache
+
+    def get_or_create_result(self, result_cache, t, cell_id, result_key):
         substrate_id = result_key[0]
         energy_id = result_key[1]
         gear_id = result_key[2]
         feature_id = result_key[3]
 
-        if not self.results_t_c[t][cell_id].has_key(result_key):
-            new_result = {
-                't': t,
-                'cell_id' : cell_id,
-                'gear_id' : gear_id,
-                'substrate_id': substrate_id,
-                'energy_id': energy_id,
-                'feature_id' : feature_id,
-                'a': 0.0,
-                'x': 0.0,
-                'y': 0.0,
-                'z': 0.0,
-                'znet': 0.0
-            }
-            self.results_t_c[t][cell_id][result_key] = new_result
-            self.results.append(new_result)
+        if not result_cache[t].has_key(result_key):
+            new_result = self.dao.schema['sources']['Result'](
+                t=t,
+                cell_id=cell_id,
+                gear_id=gear_id,
+                substrate_id=substrate_id,
+                energy_id=energy_id,
+                feature_id=feature_id,
+                a=0.0,
+                x=0.0,
+                y=0.0,
+                z=0.0,
+                znet=0.0,
+            )
+            result_cache[t][result_key] = new_result
 
-        return self.results_t_c[t][cell_id][result_key]
+        return result_cache[t][result_key]
